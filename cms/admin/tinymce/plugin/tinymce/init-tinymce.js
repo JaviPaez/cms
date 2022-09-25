6f7dbad7d17bcee943472a14e73ebf706a63c8fd
Javier Paez <javierignacio.paez@gmail.com>
2022-09-25 11:21:32 -0300

    modifying tinymce textarea

--- cms/admin/tinymce/plugin/tinymce/init-tinymce.js
+++ cms/admin/tinymce/plugin/tinymce/init-tinymce.js
@@ -1,4 +1,18 @@
 tinymce.init({
+    /* replace textarea having class .tinymce with tinymce editor */
     selector: "textarea.tinymce",
-    language: "es_MX"
+    language: "es_MX",
+
+    /* toolbar */
+    toolbar: "insertfile undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons | code",
+
+    /* statusbar */
+    //statusbar: false
+
+    /* additional plugins */
+    plugins: 'code',
+
+    /* Upgrade button */
+    promotion: false
+
 });
